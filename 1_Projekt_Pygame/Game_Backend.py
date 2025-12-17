a3d27aeb063223b3db5a645fa3d67692e996b4fe
alex132g <65401128+alex132g@users.noreply.github.com>
2025-12-17 11:09:15 +0100

    Delete 1_Projekt_Pygame directory

--- 1_Projekt_Pygame/Game_Backend.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, x, y, radius, speed, color):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.move_speed = speed
-        self.color = color
-        
